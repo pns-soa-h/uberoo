08cd8790bcd045abd05298dc4052e7cc2f49675d
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-09-29 23:59:03 +0200

    First try to integrate tags in meals

--- uberoo-meals/src/main/java/fr/unice/polytech/soa/uberoo/model/Tag.java
+++ uberoo-meals/src/main/java/fr/unice/polytech/soa/uberoo/model/Tag.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 /**
  * Created by Alexis Couvreur on 9/24/2018.
@@ -16,6 +17,8 @@ public class Tag {
     private Long id;
     private String label;
 
+    public Tag() { this.label = ""; }
+
     public Tag(String label) {
         this.label = label;
     }
@@ -26,5 +29,18 @@ public class Tag {
 
     public String getLabel() {
         return label;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(label, tag.label);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label);
     }
 }
