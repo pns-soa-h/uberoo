901c477e19f5e6c1c8bb10763bb3e5114e624469
Ivan Picard Marchetto <ivan.picardmarchetto@gmail.com>
2018-10-02 22:49:13 +0200

    TimeETA

--- uberoo-orders/src/main/java/fr/unice/polytech/soa/uberoo/TimeETA.java
+++ uberoo-orders/src/main/java/fr/unice/polytech/soa/uberoo/TimeETA.java
@@ -1,18 +1,15 @@
 package fr.unice.polytech.soa.uberoo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import fr.unice.polytech.soa.uberoo.model.Coursier;
+import fr.unice.polytech.soa.uberoo.model.Order;
 
 /**
  * Created by Picard Marchetto Ivan on 10/1/2018.
  */
-@RestController
 public interface TimeETA {
-
-	@GetMapping("/timeETA/{idClient}/{idCoursier}/{idMeal}")
-	long calculateOrderETA(@PathVariable("idClient") long idClient,@PathVariable("idCoursier") long idCoursier,
-			@PathVariable("idMeal") long idMeal);
+	
+	//Return a time in milisecond
+	long calculateOrderETA(Order O, Coursier c);
 	
 	
 }
