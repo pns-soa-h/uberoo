49026f16feda865f98dfb46ae8f69004fc7dbd50
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-10-04 16:30:30 +0200

    Can get individual meals

--- uberoo-meals/src/main/java/fr/unice/polytech/soa/uberoo/controller/MealController.java
+++ uberoo-meals/src/main/java/fr/unice/polytech/soa/uberoo/controller/MealController.java
@@ -1,11 +1,13 @@
 package fr.unice.polytech.soa.uberoo.controller;
 
 import fr.unice.polytech.soa.uberoo.assembler.MealResourceAssembler;
+import fr.unice.polytech.soa.uberoo.exception.MealNotFoundException;
 import fr.unice.polytech.soa.uberoo.model.Meal;
 import fr.unice.polytech.soa.uberoo.repository.MealRepository;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +47,12 @@ public class MealController {
 
 		return new Resources<>(meals,
 				linkTo(methodOn(MealController.class).getMeals(null)).withSelfRel());
+	}
+
+	@GetMapping("/meals/{id}")
+	public Resource<Meal> getMeal(@PathVariable Long id) {
+		Meal meal = repository.findById(id).orElseThrow(() -> new MealNotFoundException(id));
+
+		return resourceAssembler.toResource(meal);
 	}
 }
