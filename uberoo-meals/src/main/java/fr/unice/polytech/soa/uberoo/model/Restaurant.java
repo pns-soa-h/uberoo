e73de15a2a245ee80605a3df2a53478e340b9d9d
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-10-06 21:54:33 +0200

    Link meals to a restaurant

--- uberoo-meals/src/main/java/fr/unice/polytech/soa/uberoo/model/Restaurant.java
+++ uberoo-meals/src/main/java/fr/unice/polytech/soa/uberoo/model/Restaurant.java
@@ -1,9 +1,6 @@
 package fr.unice.polytech.soa.uberoo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by Alexis Couvreur on 9/24/2018.
@@ -14,7 +11,12 @@ public class Restaurant {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
+	@Column(unique = true)
     private String name;
+
+	public Restaurant() {
+
+	}
 
     public Restaurant(String name) {
         this.name = name;
