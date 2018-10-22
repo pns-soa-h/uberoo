549da316ae7063754532e46b3289b059f63749bd
Ivan Picard Marchetto <ivan.picard-marchetto@etu.unice.fr>
2018-10-22 15:31:40 +0200

    did some stuff

--- uberoo-payments/src/main/java/fr/unice/polytech/soa/uberoo/controller/PaymentController.java
+++ uberoo-payments/src/main/java/fr/unice/polytech/soa/uberoo/controller/PaymentController.java
@@ -1,8 +1,12 @@
 package fr.unice.polytech.soa.uberoo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.unice.polytech.soa.uberoo.exception.OrderNotFoundException;
+import fr.unice.polytech.soa.uberoo.model.Order;
 import fr.unice.polytech.soa.uberoo.repository.OrderRepository;
 import fr.unice.polytech.soa.uberoo.repository.RestaurantRepository;
 
@@ -16,6 +20,11 @@ public class PaymentController {
 	public PaymentController(OrderRepository orderRepository, RestaurantRepository restaurantRepository) {
 		this.orderRepository = orderRepository;
 		this.restaurantRepository = restaurantRepository;
+	}
+	
+	@GetMapping("/payments/{id}")
+	public void payOrder(@PathVariable Long id) {
+		Order o = orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
 	}
 	
 	
