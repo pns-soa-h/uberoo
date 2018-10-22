41694fa35d86edc1aa565d0126837a2257d7ecd9
Johann Mortara <johann.mortara@etu.unice.fr>
2018-10-22 20:52:54 +0200

    Dockerized load tests using Gatling.

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 mvn clean package
 docker-compose build
 docker build -t uberoo-scenario ./uberoo-scenario/docker
+docker build -t uberoo-load ./uberoo-load/docker
+
---
+++ load.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -i --rm --network host uberoo-load
