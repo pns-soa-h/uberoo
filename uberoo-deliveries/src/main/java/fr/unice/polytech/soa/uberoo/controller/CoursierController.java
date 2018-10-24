cb7ca8117bb2b3c4b8054eb931640d74453445ef
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-10-24 21:20:01 +0200

    Filter coursiers by name

--- uberoo-deliveries/src/main/java/fr/unice/polytech/soa/uberoo/controller/CoursierController.java
+++ uberoo-deliveries/src/main/java/fr/unice/polytech/soa/uberoo/controller/CoursierController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -28,12 +30,18 @@ public class CoursierController {
 	}
 
 	@GetMapping("/coursiers")
-	public Resources<Resource<Coursier>> all() {
-		List<Resource<Coursier>> coursiers = repository.findAll().stream()
+	public Resources<Resource<Coursier>> all(@RequestParam(value = "name", required = false)String name) {
+		Stream<Coursier> coursiersStream = repository.findAll().stream();
+
+		if (name != null) {
+			coursiersStream = coursiersStream.filter(c -> c.getName().toLowerCase().equals(name.toLowerCase()));
+		}
+
+		List<Resource<Coursier>> coursiers = coursiersStream
 				.map(assembler::toResource)
 				.collect(Collectors.toList());
 
 		return new Resources<>(coursiers,
-			linkTo(methodOn(CoursierController.class).all()).withSelfRel());
+			linkTo(methodOn(CoursierController.class).all(null)).withSelfRel());
 	}
 }
