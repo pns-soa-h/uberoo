02afafc0bbf307a592daf91af4b132fd7435227e
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-10-25 08:19:01 +0200

    Load one coursier

--- uberoo-deliveries/src/main/java/fr/unice/polytech/soa/uberoo/assembler/CoursierResourceAssembler.java
+++ uberoo-deliveries/src/main/java/fr/unice/polytech/soa/uberoo/assembler/CoursierResourceAssembler.java
@@ -1,9 +1,13 @@
 package fr.unice.polytech.soa.uberoo.assembler;
 
+import fr.unice.polytech.soa.uberoo.controller.CoursierController;
 import fr.unice.polytech.soa.uberoo.model.Coursier;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  *
@@ -14,7 +18,8 @@ public class CoursierResourceAssembler implements ResourceAssembler<Coursier, Re
 
 	@Override
 	public Resource<Coursier> toResource(Coursier coursier) {
-		return new Resource<>(coursier
-				);
+		return new Resource<>(coursier,
+				linkTo(methodOn(CoursierController.class).one(coursier.getId())).withSelfRel(),
+				linkTo(methodOn(CoursierController.class).all(null)).withRel("coursiers"));
 	}
 }
