05e73984436ad24d767ebc0ee5ed92050ba42396
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-10-25 08:19:01 +0200

    Load one coursier

--- uberoo-deliveries/src/main/java/fr/unice/polytech/soa/uberoo/controller/CoursierController.java
+++ uberoo-deliveries/src/main/java/fr/unice/polytech/soa/uberoo/controller/CoursierController.java
@@ -1,12 +1,14 @@
 package fr.unice.polytech.soa.uberoo.controller;
 
 import fr.unice.polytech.soa.uberoo.assembler.CoursierResourceAssembler;
+import fr.unice.polytech.soa.uberoo.exception.CoursierNotFoundException;
 import fr.unice.polytech.soa.uberoo.model.Coursier;
 import fr.unice.polytech.soa.uberoo.repository.CoursierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +45,12 @@ public class CoursierController {
 
 		return new Resources<>(coursiers,
 			linkTo(methodOn(CoursierController.class).all(null)).withSelfRel());
+	}
+
+	@GetMapping("/coursiers/{id}")
+	public Resource<Coursier> one(@PathVariable Long id) {
+		Coursier coursier = repository.findById(id).orElseThrow(() -> new CoursierNotFoundException(id));
+
+		return assembler.toResource(coursier);
 	}
 }
