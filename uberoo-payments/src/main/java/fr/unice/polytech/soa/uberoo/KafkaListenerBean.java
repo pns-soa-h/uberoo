05bd764df6f541efc031b0aa310fdc8fd3eebb3f
Ivan Picard Marchetto <ivan.picard-marchetto@etu.unice.fr>
2018-10-29 15:55:43 +0100

    message stuff

--- uberoo-payments/src/main/java/fr/unice/polytech/soa/uberoo/KafkaListenerBean.java
+++ uberoo-payments/src/main/java/fr/unice/polytech/soa/uberoo/KafkaListenerBean.java
@@ -2,11 +2,15 @@ package fr.unice.polytech.soa.uberoo;
 
 import fr.unice.polytech.soa.uberoo.model.Addition;
 import fr.unice.polytech.soa.uberoo.model.Order;
+import fr.unice.polytech.soa.uberoo.model.Recu;
 import fr.unice.polytech.soa.uberoo.repository.OrderRepository;
 import fr.unice.polytech.soa.uberoo.repository.RecuRepository;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +25,9 @@ public class KafkaListenerBean {
 	@Autowired
 	private RecuRepository recuRepository;
 	
+	private KafkaTemplate<String, Recu> kafkaTemplate;
+	
+	
 	@KafkaListener(topics = "payment")
 	public void deliver(Addition payment, Acknowledgment acknowledgment) {
 		acknowledgment.acknowledge();
@@ -33,6 +40,7 @@ public class KafkaListenerBean {
 			//do alreadt paid;
 			return;
 		}
+		kafkaTemplate.send("recu", new Recu(payment.getOrderId(),payment.getMontant(),new Date(System.currentTimeMillis())));
 		
 	}
 
--- uberoo-payments/src/main/java/fr/unice/polytech/soa/uberoo/model/Recu.java
+++ uberoo-payments/src/main/java/fr/unice/polytech/soa/uberoo/model/Recu.java
@@ -11,5 +11,29 @@ public class Recu {
 	private Long orderId;
 	private float montant;
 	private Date timestamp;
+	public Recu(Long orderId, float montant, Date timestamp) {
+		super();
+		this.orderId = orderId;
+		this.montant = montant;
+		this.timestamp = timestamp;
+	}
+	public Long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+	public float getMontant() {
+		return montant;
+	}
+	public void setMontant(float montant) {
+		this.montant = montant;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
 
 }
