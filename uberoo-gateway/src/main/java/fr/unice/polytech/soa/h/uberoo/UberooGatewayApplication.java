1e29ec1d5eb694999a62c44d591c018fcfe55ff8
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-11-04 10:47:09 +0100

    Uberoo gateway module created

---
+++ uberoo-gateway/src/main/java/fr/unice/polytech/soa/h/uberoo/UberooGatewayApplication.java
@@ -0,0 +1,12 @@
+package fr.unice.polytech.soa.h.uberoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberooGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberooGatewayApplication.class, args);
+	}
+}
