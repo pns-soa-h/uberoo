92d986b5128cc04e59e5b69f1c66cfdc47d8717e
Johann Mortara <johann.mortara@etu.unice.fr>
2018-11-09 22:34:16 +0100

    Failing but compiling test

--- uberoo-load/gatling/user-files/simulations/Simulation.scala
+++ uberoo-load/gatling/user-files/simulations/Simulation.scala
@@ -2,6 +2,9 @@ import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
+import scala.concurrent.duration._
+
+import java.util.UUID
 
 class LoadTest extends Simulation {
 
@@ -10,14 +13,19 @@ class LoadTest extends Simulation {
   val deliveries_url = "http://localhost:8282"
 
   val consultMeals = scenario("Consult meals")
-    .exec(http("Get meals")
-      .get(meals_url + "/meals")
-      .check(jsonPath("$")
-        .saveAs("mealList")))
-  //    .pause(1)
-  //    .exec(http("Compute ETA")
-  //      .post(eta_url + "/eta")
-  //      .body(StringBody("""{ "calculateETA": { "meals": ${mealList} } }""")))
+	.repeat(10)
+	{
+		exec(session => session.set("clientId", UUID.randomUUID().toString))
+		.exec(http("Get meals").get("/meals"))
+			.pause(5)
+		.exec(http("ETA computation").post(orders_url + "/orders")
+			.body(StringBody(session => buildRetrieve(session))))
+	}
+    
+  def buildRetrieve(session: Session): String = {
+  	val clientId = session("clientId").as[String]
+  	raw"""'{"clientId":"'$clientId'", "mealId":"1", "restaurantId":"1"}'"""
+  }
 
-  setUp(consultMeals.inject(atOnceUsers(1)))
+  setUp(consultMeals.inject(rampUsers(10) over (10 seconds)))
 }
