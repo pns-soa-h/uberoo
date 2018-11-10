11e2e20c6f8fd6b0a6072f4b0e2f07f148e4d4c0
Johann Mortara <johann.mortara@etu.unice.fr>
2018-11-10 15:25:14 +0100

    Functional load test

--- uberoo-load/gatling/user-files/simulations/Simulation.scala
+++ uberoo-load/gatling/user-files/simulations/Simulation.scala
@@ -10,22 +10,22 @@ class LoadTest extends Simulation {
 
   val meals_url = "http://localhost:8080"
   val orders_url = "http://localhost:8181"
-  val deliveries_url = "http://localhost:8282"
 
   val consultMeals = scenario("Consult meals")
-	.repeat(10)
-	{
-		exec(session => session.set("clientId", UUID.randomUUID().toString))
-		.exec(http("Get meals").get("/meals"))
-			.pause(5)
-		.exec(http("ETA computation").post(orders_url + "/orders")
-			.body(StringBody(session => buildRetrieve(session))))
-	}
-    
-  def buildRetrieve(session: Session): String = {
-  	val clientId = session("clientId").as[String]
-  	raw"""'{"clientId":"'$clientId'", "mealId":"1", "restaurantId":"1"}'"""
-  }
+    .repeat(5) {
+      exec(session => session.set("clientId", UUID.randomUUID().getMostSignificantBits().toString))
+        .exec(http("Get meals").get(meals_url + "/meals"))
+        .pause(5)
+        .exec(http("ETA computation").post(orders_url + "/orders")
+          .header("Content-Type", "application/json")
+          .body(StringBody(session => buildRetrieve(session)))
+          .asJSON)
+    }
 
-  setUp(consultMeals.inject(rampUsers(10) over (10 seconds)))
+  def buildRetrieve(session: Session): String = {
+    val clientId = session("clientId").as[String]
+    raw"""{"clientId":"$clientId", "mealId":"1", "restaurantId":"1"}"""
+  }
+  // Ok up to 730, breaks at 740
+  setUp(consultMeals.inject(rampUsers(730) over (2 minutes)))
 }
