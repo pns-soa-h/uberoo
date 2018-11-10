97524a08b9f5b36e0ca97b584afa5feb70079aab
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2018-11-11 00:23:51 +0100

    Fixed tests for meals !

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mvn clean package -DskipTests=true
+mvn clean package
 docker-compose build
 docker build -t uberoo-scenario ./uberoo-scenario/docker
 docker build -t uberoo-load ./uberoo-load/docker
