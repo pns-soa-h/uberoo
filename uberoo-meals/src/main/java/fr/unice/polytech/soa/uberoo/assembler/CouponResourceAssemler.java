0b58431d85a6f9351c61f29c11794f8a76c08f87
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2018-11-11 14:56:32 +0100

    Added rel

--- uberoo-meals/src/main/java/fr/unice/polytech/soa/uberoo/assembler/CouponResourceAssemler.java
+++ uberoo-meals/src/main/java/fr/unice/polytech/soa/uberoo/assembler/CouponResourceAssemler.java
@@ -1,16 +1,19 @@
 package fr.unice.polytech.soa.uberoo.assembler;
 
+import fr.unice.polytech.soa.uberoo.controller.CouponController;
 import fr.unice.polytech.soa.uberoo.model.Coupon;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 @Component
 public class CouponResourceAssemler implements ResourceAssembler<Coupon, Resource<Coupon>> {
 	@Override
 	public Resource<Coupon> toResource(Coupon entity) {
-		return new Resource<>(entity/*,
-				linkTo(methodOn(CouponController.class).getMeal(meal.getId())).withSelfRel(),
-				linkTo(methodOn(CouponController.class).getMeals(null)).withRel("meals")*/);
+		return new Resource<>(entity,
+				linkTo(methodOn(CouponController.class).findAll(null)).withRel("coupons"));
 	}
 }
